2a8b6058860f0315535e09f7e6d7a4d8f4b7d464
Maesawa-code <hirokitechcamp@gmail.com>
2025-04-16 16:55:30 +0900

    20240416 - ホーム画面のデザイン - 完了

--- app/resources/views/home.blade.php
+++ app/resources/views/home.blade.php
@@ -1,22 +1,30 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<div class="container py-5">
+    <div class="row g-5">
+        <div class="col-md-4">
+            <a href="#" class="btn btn-warning text-dark fw-bold fs-3 py-5 w-100">
+                在庫一覧
+            </a>
+        </div>
+        <div class="col-md-4">
+            <a href="#" class="btn btn-warning text-dark fw-bold fs-3 py-5 w-100">
+                入荷予定
+            </a>
+        </div>
+        <div class="col-md-4">
+            <a href="#" class="btn btn-warning text-dark fw-bold fs-3 py-5 w-100">
+                在庫一覧（全店舗）
+            </a>
+        </div>
+    </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <div class="row g-5 justify-content-start mt-2">
+        <div class="col-md-4">
+            <a href="#" class="btn btn-warning text-dark fw-bold fs-3 py-5 w-100">
+                社員
+            </a>
         </div>
     </div>
 </div>
