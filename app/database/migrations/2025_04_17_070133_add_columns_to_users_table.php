f20e4adc1091b08acdf92d922e402e8b4662561e
Maesawa-code <hirokitechcamp@gmail.com>
2025-04-17 16:28:57 +0900

    20250417 - ユーザーテーブルに店舗・役職カラム追加 - 完了

---
+++ app/database/migrations/2025_04_17_070133_add_columns_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+
+            $table->softDeletes();
+
+            $table->foreign('store_id')->references('id')->on('stores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            
+            $table->dropForeign(['store_id']);
+            $table->dropColumn(['role', 'store_id', 'deleted_at']);
+        });
+    }
+}
